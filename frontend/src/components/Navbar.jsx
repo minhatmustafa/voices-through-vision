84affc8dca5228ad3af65cacd0e2bcdbd1a1c1ad
Minhat Mustafa <mustafa.minnie@gmail.com>
2026-03-08 15:00:58 -0500

    fixed donate page in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,7 +15,7 @@ export default function Navbar() {
           <Link to="/our-vision" className="hover:underline">Our Vision</Link>
           <Link to="/our-team" className="hover:underline">Our Team</Link>
           <Link to="/volunteer-with-us" className="hover:underline">Volunteer</Link>
-          <a href="/donate" className="px-4 py-1.5 text-white transition" style={{ backgroundColor: '#7d9b76' }}>Donate</a>
+          <Link to="/donate" className="px-4 py-1.5 text-white transition" style={{ backgroundColor: '#7d9b76' }}>Donate</Link>
         </div>
 
         {/* Hamburger button */}
